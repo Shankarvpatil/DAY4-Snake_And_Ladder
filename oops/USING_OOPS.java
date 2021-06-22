fea6acfc75fbd0f05541360061031e06de85a3a5
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-22 23:31:41 +0530

    Update USING_OOPS.java

--- oops/USING_OOPS.java
+++ oops/USING_OOPS.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
-public class UC3_NOPlay_Ladder_Snake {
+public class UC4_Winning_Position_Hundred {
+	public static final int Winning = 100;
 	public static void main(String[] args) {
 		System.out.print("Enter player name: ");
 		Scanner s = new Scanner(System.in);
 		String Name = s.next();
 		int Position = 0;
-		System.out.println("Player name is: " + Name);
+		System.out.println("Player name: " + Name);
+		
+		while ( Winning > Position ) {
 		int Dice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-		System.out.println("Dice roll: " + Dice);
+			System.out.println(Dice);
 		
 		int number = (int) (Math.floor(Math.random() * 10) % 3);
+			System.out.println(number);
 		switch (number) {
 			case 0:
-				System.out.println("No play");
-				Position += 0;
+				System.out.println("No play : "+ 0);
+				Position = Position + 0;
 				break;
 			case 1:
-				System.out.println("Ladder");
-				Position += Dice;
+				System.out.println("Ladder  : "+ Dice);
+				Position = Position + Dice;
 				break;
 			default:
-				System.out.println("Snake");
-				Position -= Dice;
+				System.out.println("Snake   : "+ Dice);
+				Position = Position - Dice;
 				if (Position < 0)
-					Position = 0;		
+					Position = 0;
 		}
-		System.out.println("Player position is: " + Position);
+		}
+		System.out.println("Player position is : " + Position);
 	}
 }
