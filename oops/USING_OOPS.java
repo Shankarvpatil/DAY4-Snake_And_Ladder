347377e1b0707870451d2cd7c1832130bbd16d6b
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-23 00:28:36 +0530

    Update USING_OOPS.java

--- oops/USING_OOPS.java
+++ oops/USING_OOPS.java
@@ -1,39 +1,38 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 import java.util.Scanner;
-public class UC5_EXACT_WINNING_POSITION_HUNDRED {
+public class UC6 {
 	public static final int Winning = 100;
 	public static void main(String[] args) {
 		System.out.print("Enter player name: ");
 		Scanner s = new Scanner(System.in);
 		String Name = s.next();
 		int Position = 0;
-		System.out.println("Player name: " + Name);
+		int DiceCount = 0;
+		System.out.println("Player name is: " + Name);
 		while ( Winning > Position ) {
 		int Dice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-		System.out.println(Dice);
+			System.out.println(Dice);
+		DiceCount++;
+			System.out.println(DiceCount);
 		int remaining = Winning - Position;
-		System.out.println(remaining);
+			System.out.println(remaining);
 		int number = (int) (Math.floor(Math.random() * 10) % 3);
-		System.out.println(number);
+			System.out.println(number);
 		if( remaining >= Dice ) {
-			switch (number) {
-				case 0:
-					System.out.println("No play : " + 0);
-					Position = Position + 0;
-					break;
-				case 1:
-					System.out.println("Ladder  : "+ Dice);
-					Position = Position + Dice;
-					break;
-				default:
-					System.out.println("Snake   : "+ Dice);
-					Position = Position - Dice;
-					if (Position < 0)
-						Position = 0;
-			}
-		}
-		}
-		System.out.println("Player position: " + Position);
+		switch (number) {
+			case 0:
+				Position = Position + 0;
+				break;
+			case 1:
+				Position = Position + Dice;
+				break;
+			default:
+				Position = Position - Dice;
+				if (Position < 0)
+					Position = 0;
+		} 
+		} 
+		System.out.println("Player position is : " + Position);
+		} 
+		//System.out.println("Dice rolled " + DiceCount + " times.");
 	}
 }
