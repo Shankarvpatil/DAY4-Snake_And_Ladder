ff728ae43ff3d2a7a4ab6f4f679d680c7e4e5c1a
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-22 21:16:18 +0530

    Update USING_OOPS.java

--- oops/USING_OOPS.java
+++ oops/USING_OOPS.java
@@ -1,8 +1,8 @@
-public class UC1_Start_position {
+public class UC2_Random_Six_die {
 	public static void main(String[] args) {
-		System.out.println("Welcome to GAME");
-		int Position = 0;	
-		System.out.println("Starting position of player is " + Position );
+		int Position = 0;
+		int Dice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		System.out.println("Dice roll: " + Dice);		
 	}
 }
 
