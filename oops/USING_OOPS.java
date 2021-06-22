4cd4c08dfa7c56df10bb517e9084698dd77598e2
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-22 21:22:20 +0530

    Update USING_OOPS.java

--- oops/USING_OOPS.java
+++ oops/USING_OOPS.java
@@ -1,8 +1,30 @@
-public class UC2_Random_Six_die {
+import java.util.Scanner;
+public class UC3_NOPlay_Ladder_Snake {
 	public static void main(String[] args) {
+		System.out.print("Enter player name: ");
+		Scanner s = new Scanner(System.in);
+		String Name = s.next();
 		int Position = 0;
+		System.out.println("Player name is: " + Name);
 		int Dice = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-		System.out.println("Dice roll: " + Dice);		
+		System.out.println("Dice roll: " + Dice);
+		
+		int number = (int) (Math.floor(Math.random() * 10) % 3);
+		switch (number) {
+			case 0:
+				System.out.println("No play");
+				Position += 0;
+				break;
+			case 1:
+				System.out.println("Ladder");
+				Position += Dice;
+				break;
+			default:
+				System.out.println("Snake");
+				Position -= Dice;
+				if (Position < 0)
+					Position = 0;		
+		}
+		System.out.println("Player position is: " + Position);
 	}
 }
-
