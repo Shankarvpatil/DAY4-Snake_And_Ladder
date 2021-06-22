d4276db2da7f6f65ec62a71dc9f42dfaea0e5047
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-22 21:13:37 +0530

    Update USING_OOPS.java

--- oops/USING_OOPS.java
+++ oops/USING_OOPS.java
@@ -1,2 +1,8 @@
-#oops
+public class UC1_Start_position {
+	public static void main(String[] args) {
+		System.out.println("Welcome to GAME");
+		int Position = 0;	
+		System.out.println("Starting position of player is " + Position );
+	}
+}
 
